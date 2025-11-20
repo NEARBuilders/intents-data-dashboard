dc28703f07d06f59be56c5e4cf36f28831bd6413
Elliot Braem <elliot@ejlbraem.com>
2025-11-20 14:50:49 -0600

    update vite build

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -4,18 +4,21 @@ import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import viteReact from '@vitejs/plugin-react'
 import viteTsConfigPaths from 'vite-tsconfig-paths'
 import tailwindcss from '@tailwindcss/vite'
-import { nitro } from 'nitro/vite'
+import { nitroV2Plugin } from '@tanstack/nitro-v2-vite-plugin'
 
 const config = defineConfig({
   plugins: [
     devtools(),
-    nitro() as any,
     // this is the plugin that enables path aliases
     viteTsConfigPaths({
       projects: ['./tsconfig.json'],
     }),
     tailwindcss(),
     tanstackStart(),
+    nitroV2Plugin({
+      preset: 'bun',
+      compatibilityDate: 'latest'
+    }),
     viteReact(),
   ],
 })
